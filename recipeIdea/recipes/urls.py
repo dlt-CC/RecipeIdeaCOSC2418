e7903180da12407001da9ac015711d89a58eb2cc
JustinQuanCC <justin.quan@mycc.caspercollege.edu>
2022-12-01 15:08:55 -0700

    general additions
    added new tabs on the navbar
    static folder for site.css
    new urls for new pages
    new html placeholders
    removed parts of or changed formatting of base.html

--- recipeIdea/recipes/urls.py
+++ recipeIdea/recipes/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('login/', views.Login, name='login'),
+    path('register/', views.Register, name='register'),
+    path('register/', views.add, name='add'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
