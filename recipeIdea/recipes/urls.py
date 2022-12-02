6ee6b9c8a9f5cd727c9b4f7fa2fb200bd04e0df7
MasonHall-hub <116520060+MasonHall-hub@users.noreply.github.com>
2022-12-02 13:50:14 -0700

    Add files via upload

--- recipeIdea/recipes/urls.py
+++ recipeIdea/recipes/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('login/', views.Login, name='login'),
-    path('register/', views.Register, name='register'),
-    path('register/', views.add, name='add'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/comment/', views.comment, name='comment')
 ]
--- recipeIdea/recipes/views.py
+++ recipeIdea/recipes/views.py
@@ -1,8 +1,7 @@
 from django.views import generic
 from .models import Post
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
+from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -11,15 +10,27 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+    
+def comment(request, slug):
+    template_name = 'comment.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
 
-def Register(request):
-    template = loader.get_template('register.html')
-    return HttpResponse(template.render())
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
 
-def Login(request):
-    template = loader.get_template('login.html')
-    return HttpResponse(template.render())
-
-def add(request):
-    template = loader.get_template('add.html')
-    return HttpResponse(template.render())
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
