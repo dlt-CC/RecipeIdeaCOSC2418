851286943c12fe9d1b9f3783a657113b2f37e861
dlt-CC <116520502+dlt-CC@users.noreply.github.com>
2022-12-08 17:15:46 -0700

    settings.py file
    Delete duplicate recipe entry in the settings.py file that we talked about and you approved, but it needed to be deleted.

--- recipeIdea/recipeIdea/settings.py
+++ recipeIdea/recipeIdea/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'recipes'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
