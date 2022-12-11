5936062220c94594726950d76c9935227b1424c6
dlt-CC <116520502+dlt-CC@users.noreply.github.com>
2022-12-11 12:18:13 -0700

    Update forms.py
    I added labels and widgets to have the description of each box have its on label inside and outside when it is a choice.

--- recipeIdea/recipes/forms.py
+++ recipeIdea/recipes/forms.py
@@ -1,13 +1,30 @@
-from .models import Comment, Post
-from django import forms
-
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'body')
+        labels = {
+            'name': '',
+            'body': '',
+        }
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control', 'Placeholder': 'Full Name'}),
+            'body': forms.Textarea(attrs={'class':'form-control', 'Placeholder': 'Comment'}),
+        }
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'author', 'content', 'status',)
+        labels = {
+            'title': '',
+            'author': 'Author',
+            'content': '',
+            'status': '',
+        }
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'Placeholder':"Recipe Title"}),
+            'author': forms.Select(attrs={'class':'form-control', 'Placeholder':"Author"}),
+            'content': forms.Textarea(attrs={'class':'form-control', 'Placeholder':"Recipe Ingredents and Instructions"}),
+        }
